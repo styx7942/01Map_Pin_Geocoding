68e3cbec8ca7b783c290cd6502c033ecf077de5c
D7703_11 <d7703_11@d7703-11ui-imac.local>
2017-09-26 11:45:55 +0900

    Test(미완성)

--- 01Map_Pin_Geocoding/ViewController.swift
+++ 01Map_Pin_Geocoding/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        zoomToRegion()
+        
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+    
+    func zoomToRegion(){
+        let center = CLLocationCoordinate2DMake(35.166197, 129.072594)//중심
+        let span = MKCoordinateSpanMake(0.05, 0.05)//반경
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        myMapView.setRegion(region, animated: true)
     }
-
-
 }
 
